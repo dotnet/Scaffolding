635f8317ebba5d3d920800421263c8a2eb69bdc9
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2014-06-30 22:45:32 -0700

    Initial commit - just creating the project structure

---
+++ src/Microsoft.Framework.CodeGeneration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Microsoft.Framework.CodeGeneration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}

35bf8fbf6eaed398a7738f34acb068583b305838
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2014-08-14 18:30:55 -0700

    Fixing the runtests.cmd to run all tests.

--- test/runtests.cmd
+++ test/runtests.cmd
@@ -2,6 +2,7 @@ for %%1 in (
     Microsoft.Framework.CodeGeneration.Core.FunctionalTest
     Microsoft.Framework.CodeGeneration.Core.Test
     Microsoft.Framework.CodeGeneration.Templating.Test
+    Microsoft.Framework.CodeGeneration.EntityFramework.Test
     ) do (
         cd %%1
         k test

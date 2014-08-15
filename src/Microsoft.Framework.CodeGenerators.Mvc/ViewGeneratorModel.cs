fe8b7d921b96baec43e682f62e28c51fbdaaa8c7
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2014-08-15 16:26:28 -0700

    Formatting fixes. Help for partial view command line option.

--- src/Microsoft.Framework.CodeGenerators.Mvc/ViewGeneratorModel.cs
+++ src/Microsoft.Framework.CodeGenerators.Mvc/ViewGeneratorModel.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Framework.CodeGenerators.Mvc
         [Argument(Description = "The view template to use, supported view templates: 'Create|Edit|Delete|Details|List'")]
         public string TemplateName { get; set; }
 
-        [Option(Name = "partialView", ShortName = "partial")]
+        [Option(Name = "partialView", ShortName = "partial", Description = "Generate a partial view, other layout options (-l and -udl) are ignored if this is specified")]
         public bool PartialView { get; set; }
     }
 }

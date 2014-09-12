7011ef10e927a527d9596a82a747392b3c8a2dce
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2014-09-12 08:19:41 -0700

    Removing the 'samples' directory as this was just a convenient test application rather than a true sample.

--- samples/TestWebApp/CreateViews.cmd
+++
@@ -1,8 +0,0 @@
-for %%1 in (Create Edit Delete List Details) do (
-    k gen view %%1_DefaultLayout %%1 -m Person -dc PersonContext -udl -scripts -f
-    k gen view %%1_DefaultLayoutPartial %%1 -m Person -dc PersonContext -udl -partial -scripts -f
-    k gen view %%1_CustomLayout %%1 -m Person -dc PersonContext -l "MyLayout.cshtml" -scripts -f
-    k gen view %%1_CustomLayoutPartial %%1 -m Person -dc PersonContext -l "MyLayout.cshtml" -partial -scripts -f
-    k gen view %%1_NoLayout %%1 -m Person -dc PersonContext -scripts -f
-    k gen view %%1_NoLayoutPartial %%1 -m Person -dc PersonContext -partial -scripts -f
-)

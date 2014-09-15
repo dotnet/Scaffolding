6e26e0c2389d823c8db7a24de86ced22c7e4833a
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2014-09-15 15:50:07 -0700

    Responding to breaking change.

--- test/TestApps/ModelTypesLocatorTestWebApp/Startup.cs
+++ test/TestApps/ModelTypesLocatorTestWebApp/Startup.cs
@@ -5,7 +5,7 @@ namespace ModelTypesLocatorTestWebApp
 {
     public class Startup
     {
-        public void Configure(IBuilder app)
+        public void Configure(IApplicationBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         }

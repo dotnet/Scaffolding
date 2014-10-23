948020d823b40f8e80b31abf85d3e3ddbf1d3ef7
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2014-10-22 17:18:40 -0700

    Fix: https://github.com/aspnet/Scaffolding/issues/65
    The root cause is that ‘dynamic’ does not work if the underlying object is internal with beta Roslyn bits. [I know this worked in alpha4]
    
    So the fix is to simply make the class (template model used for empty template) from internal to public.
    
    [Since this used to be a shared class in a shared project, I had to remove it from shared project and make copies in the actual projects that used it]

--- src/Microsoft.Framework.CodeGenerators.Mvc/TypeUtilities.cs
+++ src/Microsoft.Framework.CodeGenerators.Mvc/TypeUtilities.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace Microsoft.Framework.CodeGeneration
+using Microsoft.Framework.CodeGeneration;
+
+namespace Microsoft.Framework.CodeGenerators.Mvc
 {
     internal static class TypeUtilities
     {

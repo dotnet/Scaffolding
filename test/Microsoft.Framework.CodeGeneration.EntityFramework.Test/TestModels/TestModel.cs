3c2723fb509f8be68913b200f167539028460010
ajcvickers <ajcvickers@hotmail.com>
2015-01-21 09:53:53 -0800

    Use convention model builder.

--- test/Microsoft.Framework.CodeGeneration.EntityFramework.Test/TestModels/TestModel.cs
+++ test/Microsoft.Framework.CodeGeneration.EntityFramework.Test/TestModels/TestModel.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Framework.CodeGeneration.EntityFramework.Test.TestModels
             get
             {
                 var model = new Model();
-                var builder = new ModelBuilder(model);
+                var builder = new ModelBuilderFactory().CreateConventionBuilder(model);
 
                 builder.Entity<Product>();
                 builder.Entity<Category>();

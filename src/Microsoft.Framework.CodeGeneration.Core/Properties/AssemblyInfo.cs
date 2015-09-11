b71439eb1fdf4cb1171713fd8f0fbdcc8ca4d05c
Pranav K <prkrishn@hotmail.com>
2015-09-10 18:33:31 -0700

    Adding NeutralResourcesLanguageAttribute

--- src/Microsoft.Framework.CodeGeneration.Core/Properties/AssemblyInfo.cs
+++ src/Microsoft.Framework.CodeGeneration.Core/Properties/AssemblyInfo.cs
@@ -2,8 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.Framework.CodeGeneration.Core.Test")]
 [assembly: InternalsVisibleTo("Microsoft.Framework.CodeGeneration.EntityFramework.Test")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-us")]

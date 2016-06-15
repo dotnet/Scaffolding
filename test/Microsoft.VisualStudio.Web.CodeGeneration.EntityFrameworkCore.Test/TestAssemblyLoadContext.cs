ca96b81b3ec23bc17ae875f87dc94315ad1fc2f9
Prafull Bhosale <prbhosal@microsoft.com>
2016-06-15 15:41:15 -0700

    Look for model type in dependencies if its not found in the project directly. Fixes #251

---
+++ test/Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.Test/TestAssemblyLoadContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.DotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.Test
+{
+    public class TestAssemblyLoadContext : ICodeGenAssemblyLoadContext
+    {
+        private ICodeGenAssemblyLoadContext _defaultContext;
+        ILibraryManager _manager;
+        ILibraryExporter _exporter;
+        public TestAssemblyLoadContext(ILibraryExporter exporter, ILibraryManager manager)
+        {
+            _manager = manager;
+            _exporter = exporter;
+            _defaultContext = new DefaultAssemblyLoadContext();
+        }
+        public Assembly LoadFromName(AssemblyName AssemblyName)
+        {
+            var library = _manager.GetLibrary(AssemblyName.Name);
+            var path = _exporter.GetResolvedPathForDependency(library);
+            return AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
+        }
+
+        public Assembly LoadStream(Stream assembly, Stream symbols)
+        {
+            return _defaultContext.LoadStream(assembly, symbols);
+        }
+    }
+}
--- test/TestApps/ModelTypesLocatorTestWebApp/Startup.cs
+++ test/TestApps/ModelTypesLocatorTestWebApp/Startup.cs
@@ -2,12 +2,28 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ModelTypesLocatorTestWebApp.Models;
 
 namespace ModelTypesLocatorTestWebApp
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CarContext>(options =>
+                    options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CarContext-3ae1a974-3117-4483-853a-06d90fdb3bd0;Trusted_Connection=True;MultipleActiveResultSets=true"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
         }
     }

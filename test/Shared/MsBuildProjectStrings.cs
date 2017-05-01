915522c1380f5541432fd594ea4b2d98999d227d
Prafull Bhosale <prbhosal@microsoft.com>
2017-05-01 14:24:17 -0700

    Change params from DbContextFactoryOptions to string [].

--- test/Shared/MsBuildProjectStrings.cs
+++ test/Shared/MsBuildProjectStrings.cs
@@ -12,8 +12,8 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration
     <packageSources>
         <clear />
         <add key=""local"" value=""" + artifactsDir +  @""" />
-        <add key=""AspNetCoreCiDev"" value=""https://dotnet.myget.org/F/aspnetcore-ci-dev/api/v3/index.json"" />
-        <add key=""dotnet-core"" value=""https://dotnet.myget.org/F/dotnet-core/api/v3/index.json"" />
+        <add key=""AspNetCore"" value=""https://dotnet.myget.org/F/aspnetcore-ci-release/api/v3/index.json"" />
+        <add key=""cli-deps"" value=""https://dotnet.myget.org/F/cli-deps/api/v3/index.json"" />
         <add key=""NuGet"" value=""https://api.nuget.org/v3/index.json"" />
     </packageSources>
 </configuration>";

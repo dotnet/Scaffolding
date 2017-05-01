da0e1200f67d9d0d4446d8456ad15202749cd698
Prafull Bhosale <prbhosal@microsoft.com>
2017-05-01 11:04:49 -0700

    Fix indentation in Program.cs in test projects.

--- test/Shared/MsBuildProjectStrings.cs
+++ test/Shared/MsBuildProjectStrings.cs
@@ -260,10 +260,11 @@ namespace WebApplication1
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) => WebHost
-            .CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
 ";

a7aa9de1cd1938e83a86765f05fa60bd938dad64
Prafull Bhosale <prbhosal@microsoft.com>
2017-07-07 13:19:38 -0700

    Add ParameterDefinition file for Razor Page generator.

--- src/Microsoft.VisualStudio.Web.CodeGenerators.Mvc/RazorPage/RazorPageGeneratorModel.cs
+++ src/Microsoft.VisualStudio.Web.CodeGenerators.Mvc/RazorPage/RazorPageGeneratorModel.cs
@@ -15,10 +15,10 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Razor
         [Option(Name = "partialView", ShortName = "partial", Description = "Generate a partial view, other layout options (-l and -udl) are ignored if this is specified")]
         public bool PartialView { get; set; }
 
-        [Option(Name ="noPageModel", ShortName ="npm", Description =" Switch to not generate a PageModel class for Empty template.")]
+        [Option(Name ="noPageModel", ShortName ="npm", Description ="Switch to not generate a PageModel class for Empty template.")]
         public bool NoPageModel { get; set; }
 
-        [Option(Name ="namespaceName", ShortName ="", Description = "")]
+        [Option(Name ="namespaceName", ShortName ="namespace", Description = "Specify the name of the namespace to use for the generated PageModel")]
         public string NamespaceName { get; set; }
 
         public RazorPageGeneratorModel()

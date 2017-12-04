f662bfbd5bc60817b45375da6f1a2d2df910596b
Arthur Vickers <ajcvickers@hotmail.com>
2017-12-04 10:33:48 -0800

    Fix build break and make test more resilient to future changes.

--- test/VS.Web.CG.EFCore.Test/TestModels/TestModel.cs
+++ test/VS.Web.CG.EFCore.Test/TestModels/TestModel.cs
@@ -1,21 +1,72 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.Test.TestModels
 {
     public static class TestModel
     {
+        public class TestServiceFactory
+        {
+            public static readonly TestServiceFactory Instance = new TestServiceFactory();
+
+            private TestServiceFactory()
+            {
+            }
+
+            private readonly ConcurrentDictionary<Type, IServiceProvider> _factories
+                = new ConcurrentDictionary<Type, IServiceProvider>();
+
+            public TService Create<TService>()
+                where TService : class
+            {
+                return _factories.GetOrAdd(
+                    typeof(TService),
+                    t => AddType(new ServiceCollection(), typeof(TService)).BuildServiceProvider()).GetService<TService>();
+            }
+
+            private static ServiceCollection AddType(ServiceCollection serviceCollection, Type serviceType)
+            {
+                serviceCollection.AddSingleton(serviceType);
+
+                var constructors = serviceType.GetConstructors();
+                if (constructors.Length == 0)
+                {
+                    throw new InvalidOperationException("Cannot use with no public constructors.");
+                }
+
+                var constructor = constructors[0];
+                foreach (var candidate in constructors.Skip(1))
+                {
+                    if (candidate.GetParameters().Length > constructor.GetParameters().Length)
+                    {
+                        constructor = candidate;
+                    }
+                }
+
+                foreach (var parameter in constructor.GetParameters())
+                {
+                    AddType(serviceCollection, parameter.ParameterType);
+                }
+
+                return serviceCollection;
+            }
+        }
+
         public static IModel CategoryProductModel
         {
             get
             {
-                var sqlServerTypeMapper = new SqlServerTypeMapper(new RelationalTypeMapperDependencies()); 
+                var sqlServerTypeMapper = TestServiceFactory.Instance.Create<SqlServerTypeMapper>(); 
                 var builder = new ModelBuilder(new CoreConventionSetBuilder(new CoreConventionSetBuilderDependencies(sqlServerTypeMapper)).CreateConventionSet());
 
                 builder.Entity<Product>();
@@ -29,7 +80,7 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.Test.Tes
         {
             get
             {
-                var sqlServerTypeMapper = new SqlServerTypeMapper(new RelationalTypeMapperDependencies()); 
+                var sqlServerTypeMapper = TestServiceFactory.Instance.Create<SqlServerTypeMapper>();
                 var builder = new ModelBuilder(new CoreConventionSetBuilder(new CoreConventionSetBuilderDependencies(sqlServerTypeMapper)).CreateConventionSet());
 
                 builder.Entity<Customer>();

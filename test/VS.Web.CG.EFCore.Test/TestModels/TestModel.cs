b3abd209bae54bc846bc1ed314df4b249c10438a
Arthur Vickers <ajcvickers@hotmail.com>
2017-12-24 11:03:02 -0800

    Better fix for getting standalone SQL Server convention set
    Preempts build break from https://github.com/aspnet/EntityFrameworkCore/pull/10604

--- test/VS.Web.CG.EFCore.Test/TestModels/TestModel.cs
+++ test/VS.Web.CG.EFCore.Test/TestModels/TestModel.cs
@@ -6,7 +6,7 @@ using System.Collections.Concurrent;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,59 +15,11 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.Test.Tes
 {
     public static class TestModel
     {
-        public class TestServiceFactory
-        {
-            public static readonly TestServiceFactory Instance = new TestServiceFactory();
-
-            private TestServiceFactory()
-            {
-            }
-
-            private readonly ConcurrentDictionary<Type, IServiceProvider> _factories
-                = new ConcurrentDictionary<Type, IServiceProvider>();
-
-            public TService Create<TService>()
-                where TService : class
-            {
-                return _factories.GetOrAdd(
-                    typeof(TService),
-                    t => AddType(new ServiceCollection(), typeof(TService)).BuildServiceProvider()).GetService<TService>();
-            }
-
-            private static ServiceCollection AddType(ServiceCollection serviceCollection, Type serviceType)
-            {
-                serviceCollection.AddSingleton(serviceType);
-
-                var constructors = serviceType.GetConstructors();
-                if (constructors.Length == 0)
-                {
-                    throw new InvalidOperationException("Cannot use with no public constructors.");
-                }
-
-                var constructor = constructors[0];
-                foreach (var candidate in constructors.Skip(1))
-                {
-                    if (candidate.GetParameters().Length > constructor.GetParameters().Length)
-                    {
-                        constructor = candidate;
-                    }
-                }
-
-                foreach (var parameter in constructor.GetParameters())
-                {
-                    AddType(serviceCollection, parameter.ParameterType);
-                }
-
-                return serviceCollection;
-            }
-        }
-
         public static IModel CategoryProductModel
         {
             get
             {
-                var sqlServerTypeMapper = TestServiceFactory.Instance.Create<SqlServerTypeMapper>(); 
-                var builder = new ModelBuilder(new CoreConventionSetBuilder(new CoreConventionSetBuilderDependencies(sqlServerTypeMapper)).CreateConventionSet());
+                var builder = new ModelBuilder(SqlServerConventionSetBuilder.Build());
 
                 builder.Entity<Product>();
                 builder.Entity<Category>();
@@ -80,8 +32,7 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.Test.Tes
         {
             get
             {
-                var sqlServerTypeMapper = TestServiceFactory.Instance.Create<SqlServerTypeMapper>();
-                var builder = new ModelBuilder(new CoreConventionSetBuilder(new CoreConventionSetBuilderDependencies(sqlServerTypeMapper)).CreateConventionSet());
+                var builder = new ModelBuilder(SqlServerConventionSetBuilder.Build());
 
                 builder.Entity<Customer>();
                 builder.Entity<Order>();

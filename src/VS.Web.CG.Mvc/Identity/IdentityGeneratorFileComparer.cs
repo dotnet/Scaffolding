c6224f76c4ffae8f6029d03545c00b0fb30f822d
Prafull Bhosale <prbhosal@microsoft.com>
2018-03-23 10:52:05 -0700

    - Make _ManageNav scaffold with pages that depend on it. - Add more E2E tests to scaffold each identity page individually and ensure the project builds after scaffolding.

---
+++ src/VS.Web.CG.Mvc/Identity/IdentityGeneratorFileComparer.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
+{
+    internal class IdentityGeneratorFileComparer : IEqualityComparer<IdentityGeneratorFile>
+    {
+        public bool Equals(IdentityGeneratorFile x, IdentityGeneratorFile y)
+        {
+            // If they are the same objects or both are null.
+            if (x == y)
+            {
+                return true;
+            }
+
+            return string.Equals(x?.Name, y?.Name, StringComparison.Ordinal);
+
+        }
+
+        public int GetHashCode(IdentityGeneratorFile obj)
+        {
+            return obj?.Name?.GetHashCode() ?? 0;
+        }
+    }
+}

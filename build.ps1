59487c7fb7512637d9a6cd23dc64c4231535bbc1
John Luo <johluo@microsoft.com>
2018-03-26 12:40:57 -0700

    Update build script to set TLS version

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ErrorActionPreference = "Stop"
 
 function DownloadWithRetry([string] $url, [string] $downloadLocation, [int] $retries)

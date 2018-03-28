091597e1b94723c46e897a3dfdb3c91f4e52e49e
John Luo <johluo@microsoft.com>
2018-03-28 02:31:05 -0700

    Revert "Update build script to set TLS version"
    This reverts commit b99225cae0af90bb8fac3b632cecc6b9712236c9.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,3 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ErrorActionPreference = "Stop"
 
 function DownloadWithRetry([string] $url, [string] $downloadLocation, [int] $retries)

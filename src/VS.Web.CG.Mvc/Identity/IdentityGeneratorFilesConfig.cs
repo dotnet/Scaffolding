ca7b15c39550b16ac8286823d8b580ebc73a3f24
Sean Peters <sepeters@microsoft.com>
2018-04-20 19:01:16 -0700

    More updates to Identity page locations & content

--- src/VS.Web.CG.Mvc/Identity/IdentityGeneratorFilesConfig.cs
+++ src/VS.Web.CG.Mvc/Identity/IdentityGeneratorFilesConfig.cs
@@ -122,6 +122,17 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
             if (!string.IsNullOrEmpty(templateModel.Layout))
             {
                 filesToGenerate.Add(ViewStart);
+
+                // if there's a layout file, generate a _ValidationScriptsPartial.cshtml in the same place.
+                IdentityGeneratorFile layoutPeerValidationScriptsPartial = new IdentityGeneratorFile()
+                {
+                    Name = "_ValidationScriptsPartial",
+                    SourcePath = "Pages/_ValidationScriptsPartial.cshtml",
+                    OutputPath = Path.Combine(templateModel.SupportFileLocation, "_ValidationScriptsPartial.cshtml"),
+                    IsTemplate = false,
+                    ShouldOverWrite = OverWriteCondition.Never
+                };
+                filesToGenerate.Add(layoutPeerValidationScriptsPartial);
             }
 
             if (!templateModel.UseDefaultUI)
@@ -173,11 +184,9 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
 
             var requiredViewImports = new List<IdentityGeneratorFile>();
 
-            const string viewImportFileName = "_ViewImports";
-
             foreach (var viewImport in ViewImports)
             {
-                var fileNamePrefix = viewImport.Name.Substring(0, viewImport.Name.Length - viewImportFileName.Length);
+                var fileNamePrefix = viewImport.Name.Substring(0, viewImport.Name.Length - _ViewImportFileName.Length);
                 if (files.Any(f => f.Name.StartsWith(fileNamePrefix)))
                 {
                     if (fileSystem.FileExists(Path.Combine(rootPath, viewImport.OutputPath)))
@@ -190,6 +199,57 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
             }
 
             return requiredViewImports;
+        }
+
+        private static readonly string _ViewImportFileName = "_ViewImports";
+
+        internal static bool TryGetLayoutPeerFiles(IFileSystem fileSystem, string rootPath, IdentityGeneratorTemplateModel templateModel, out IReadOnlyList<IdentityGeneratorFile> layoutPeerFiles)
+        {
+            string viewImportsFileNameWithExtension = string.Concat(_ViewImportFileName, ".cshtml");
+
+            if (fileSystem.FileExists(Path.Combine(rootPath, templateModel.SupportFileLocation, viewImportsFileNameWithExtension)))
+            {
+                layoutPeerFiles = null;
+                return false;
+            }
+
+            const string sharedDirName = "Shared/";
+            string outputDirectory;
+            if (templateModel.SupportFileLocation.EndsWith(sharedDirName))
+            {
+                int directoryLengthWithoutShared = templateModel.SupportFileLocation.Length - sharedDirName.Length;
+                outputDirectory = templateModel.SupportFileLocation.Substring(0, directoryLengthWithoutShared);
+            }
+            else
+            {
+                outputDirectory = templateModel.SupportFileLocation;
+            }
+
+            List<IdentityGeneratorFile> peerFiles = new List<IdentityGeneratorFile>();
+
+            IdentityGeneratorFile layoutPeerViewImportsFile = new IdentityGeneratorFile()
+            {
+                Name = "_ViewImports",
+                SourcePath = "SupportPages._ViewImports.cshtml",
+                OutputPath = Path.Combine(outputDirectory, viewImportsFileNameWithExtension),
+                IsTemplate = true,
+                ShouldOverWrite = OverWriteCondition.Never
+            };
+            peerFiles.Add(layoutPeerViewImportsFile);
+
+            IdentityGeneratorFile layoutPeerViewStart = new IdentityGeneratorFile()
+            {
+                Name = "_ViewStart",
+                SourcePath = "SupportPages._ViewStart.cshtml",
+                OutputPath = Path.Combine(outputDirectory, "_ViewStart.cshtml"),
+                IsTemplate = true,
+                ShowInListFiles = false,
+                ShouldOverWrite = OverWriteCondition.Never
+            };
+            peerFiles.Add(layoutPeerViewStart);
+
+            layoutPeerFiles = peerFiles;
+            return true;
         }
 
         private static List<IdentityGeneratorFile> GetDataModelFiles(IdentityGeneratorTemplateModel templateModel)
@@ -227,9 +287,7 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
                 IsTemplate = true,
                 Name = "_LoginPartial",
                 SourcePath = "_LoginPartial.cshtml",
-                //OutputPath = "Pages/Shared/_LoginPartial.cshtml",
                 OutputPath = Path.Combine(templateModel.SupportFileLocation, "_LoginPartial.cshtml"),
-                //AltPaths = new List<string>() { "Pages/_LoginPartial.cshtml" },
                 ShouldOverWrite = OverWriteCondition.Never,
                 ShowInListFiles = false
             });

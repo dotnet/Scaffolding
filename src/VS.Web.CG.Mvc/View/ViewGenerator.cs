9bfc8164e228ad5237052da7a5f627ac601c731f
Sean Peters <sepeters@microsoft.com>
2018-08-29 14:35:03 -0700

    Bootstrap4 for all scaffolders

--- src/VS.Web.CG.Mvc/View/ViewGenerator.cs
+++ src/VS.Web.CG.Mvc/View/ViewGenerator.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.Web.CodeGeneration;
@@ -15,6 +17,21 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger _logger;
+
+        internal static readonly string DefaultBootstrapVersion = "4";
+        // A hashset would allow faster lookups, but would cause a perf hit when formatting the error string for invalid bootstrap version.
+        // Also, with a list this small, the lookup perf hit will be largely irrelevant.
+        internal static readonly IReadOnlyList<string> ValidBootstrapVersions = new List<string>()
+        {
+            "3",
+            "4"
+        };
+
+        internal static readonly string ContentVersionDefault = "Default";
+        internal static readonly string ContentVersionBootstrap3 = "Bootstrap3";
+
+        internal static readonly string DefaultContentRelativeBaseDir = "ViewGenerator";
+        internal static readonly string VersionedContentRelativeBaseDir = "ViewGenerator_Versioned";
 
         public ViewGenerator(
             IApplicationInfo applicationInfo,
@@ -92,6 +109,11 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View
             if (viewTemplate.IsModelRequired && string.IsNullOrEmpty(viewGeneratorModel.ModelClass))
             {
                 throw new InvalidOperationException(string.Format(MessageStrings.ModelClassRequiredForTemplate, templateName));
+            }
+
+            if (!string.IsNullOrEmpty(viewGeneratorModel.BootstrapVersion) && !ValidBootstrapVersions.Contains(viewGeneratorModel.BootstrapVersion))
+            {
+                throw new InvalidOperationException(string.Format(MessageStrings.InvalidBootstrapVersionForScaffolding, viewGeneratorModel.BootstrapVersion, string.Join(", ", ValidBootstrapVersions)));
             }
 
             return viewTemplate;

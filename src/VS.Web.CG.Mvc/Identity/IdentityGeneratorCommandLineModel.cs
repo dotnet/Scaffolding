817c266300ef8a60b6bb660899ee703167f57de0
Sean Peters <sepeters@microsoft.com>
2018-08-30 18:30:19 -0700

    adding the missing shoft version of the listfiles option for identity scaffolding

--- src/VS.Web.CG.Mvc/Identity/IdentityGeneratorCommandLineModel.cs
+++ src/VS.Web.CG.Mvc/Identity/IdentityGeneratorCommandLineModel.cs
@@ -19,7 +19,7 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
         [Option(Name = "files", ShortName = "fi", Description = "List of semicolon separated files to scaffold. Use the --list-files option to see the available options.")]
         public string Files { get; set; }
 
-        [Option(Name = "listFiles", Description = "Lists the files that can be scaffolded by using the '--files' option.")]
+        [Option(Name = "listFiles", ShortName ="lf", Description = "Lists the files that can be scaffolded by using the '--files' option.")]
         public bool ListFiles { get; set; }
 
         [Option(Name = "force", ShortName = "f", Description = "Use this option to overwrite existing files.")]

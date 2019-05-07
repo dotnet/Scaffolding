4038e74be1036120232354336f5296f48c214202
Sean Peters <sepeters@microsoft.com>
2019-05-06 17:30:16 -0700

    Dummy checkin for testing the build

--- src/VS.Web.CG.Design/Program.cs
+++ src/VS.Web.CG.Design/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -8,6 +8,7 @@ using Microsoft.Extensions.CommandLineUtils;
 
 namespace Microsoft.VisualStudio.Web.CodeGeneration.Design
 {
+    // Dummy comment for build/test test
     public class Program
     {
         public const string TOOL_NAME = "dotnet-aspnet-codegenerator-design";

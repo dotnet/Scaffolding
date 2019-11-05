846ad28f8c94702afd2b6ad9f9c5119cdb9568d4
Deep Choudhery <decho@microsoft.com>
2019-11-05 11:52:57 -0800

    finished basic comparison test

--- test/VS.Web.CG.Core.FunctionalTest/NupkgTest.cs
+++ test/VS.Web.CG.Core.FunctionalTest/NupkgTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -12,14 +13,18 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.Core.FunctionalTest
 {
     public class NupkgTest
     {
+        private string tfm20 = "lib/netstandard2.0";
+        //private string tfm31 = "lib/netcoreapp3.1";
+        private string tfm30 = "lib/netcoreapp3.0";
+
         [Fact]
         public void CheckFolderStructure()
         {
-            string nuget300Package = "https://www.nuget.org/api/v2/package/Microsoft.VisualStudio.Web.CodeGeneration.Design/3.0.0";
-            string nugetOrgPkg = "Microsoft.VisualStudio.Web.CodeGeneration.Design.3.0.0.nupkg";
+            string stableNugetPackagePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.nuget\\packages\\microsoft.visualstudio.web.codegeneration.design\\3.0.0\\microsoft.visualstudio.web.codegeneration.design.3.0.0.nupkg";
             string artifactsPath = "../../../../packages/Debug/Shipping/";
             DirectoryInfo taskDirectory = new DirectoryInfo(artifactsPath);
             string buildPkg = "Microsoft.VisualStudio.Web.CodeGeneration.Design*.nupkg";
+
             string fileToUse = "";
 
             foreach (var file in taskDirectory.GetFiles(buildPkg))
@@ -31,35 +36,43 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.Core.FunctionalTest
             }
 
             Assert.False(string.IsNullOrEmpty(fileToUse));
-            using (WebClient myWebClient = new WebClient())
-            {
-                myWebClient.DownloadFile(nuget300Package, fileToUse);
-            }
 
             ZipArchive zipOne, zipTwo;
-            Dictionary<string, string> nuget300files = new Dictionary<string, string>();
+            Dictionary<string, string> artifactFiles = new Dictionary<string, string>();
+            Dictionary<string, string> nuget301files = new Dictionary<string, string>();
 
-            
-            using(zipOne = ZipFile.OpenRead(fileToUse))
+            using (zipOne = ZipFile.OpenRead(fileToUse))
             {
                 foreach (ZipArchiveEntry entry in zipOne.Entries)
                 {
                     //use full path name as key due to duplicate exes, xmls.
-                    nuget300files.Add(entry.FullName, entry.Name);
+                    artifactFiles.Add(entry.FullName, entry.Name);
                 }
             }
-            using (zipTwo = ZipFile.OpenRead(nugetOrgPkg))
+
+            using (zipTwo = ZipFile.OpenRead(stableNugetPackagePath))
             {
                 foreach (ZipArchiveEntry entry in zipTwo.Entries)
                 {
                     //make sure new pkg has atleast all the old pkg files
-                    Assert.True(nuget300files.ContainsKey(entry.FullName));
-                    nuget300files.TryGetValue(entry.FullName, out string nuget300value);
-                    Assert.Equal(entry.Name, nuget300value);
-                }
-                
-            }
+                    if (artifactFiles.ContainsKey(entry.FullName))
+                    {
+                        artifactFiles.TryGetValue(entry.FullName, out string nuget300value);
+                        Assert.Equal(entry.Name, nuget300value);
+                    }
+                    else
+                    {
+                        if(entry.FullName.Contains(tfm20))
+                        {
+                            string newKey = entry.FullName.Replace(tfm20, tfm30);
+                            artifactFiles.TryGetValue(newKey, out string artifactsValue);
+                            Assert.Equal(entry.Name, artifactsValue);
+                        }
 
+                    }
+                }
+            }
         }
     }
 }
+

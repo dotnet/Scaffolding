e5dc1544af14cb56f5a497cca008ef037a7fb6b9
Deep Choudhery <decho@microsoft.com>
2020-04-03 15:05:56 -0700

    removing uneccessary projects.

--- global_install.cmd
+++ global_install.cmd
@@ -2,7 +2,6 @@ set VERSION=
 set SRC_DIR=%cd%
 set NUPKG=artifacts/packages/Debug/Shipping/
 call taskkill /f /im dotnet.exe
-call git clean -xdf
 call build.cmd  
 call dotnet tool uninstall -g dotnet-aspnet-codegenerator 
 call cd  %NUPKG% 

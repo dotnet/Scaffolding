a3147a697fe61e80c8efacc56849f787f3fefeba
Deep Choudhery <54324771+deepchoudhery@users.noreply.github.com>
2021-07-12 14:16:45 -0700

    updated all package versions and tfms to net6.0 (#1599)
    * updated all package versions and tfms to net6.0
    
    * switched dotnet sdk to previous preview

--- test/Scaffolding/Shared/MsBuildProjectSetupHelper.cs
+++ test/Scaffolding/Shared/MsBuildProjectSetupHelper.cs
@@ -57,12 +57,12 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration
         internal void SetupEmptyCodeGenerationProject(TemporaryFileProvider fileProvider, ITestOutputHelper outputHelper)
         {
             fileProvider.Add("global.json", GlobalJsonText);
-            Directory.CreateDirectory(Path.Combine(fileProvider.Root, "toolAssets", "net5.0"));
+            Directory.CreateDirectory(Path.Combine(fileProvider.Root, "toolAssets", "net6.0"));
 
             fileProvider.Add($"TestCodeGeneration.targets", MsBuildProjectStrings.ProjectContextWriterMsbuildHelperText);
 
             var msbuildTaskDllPath = Path.Combine(Path.GetDirectoryName(typeof(MsBuildProjectSetupHelper).Assembly.Location), "Microsoft.VisualStudio.Web.CodeGeneration.Msbuild.dll");
-            fileProvider.Copy(msbuildTaskDllPath, "toolAssets/net5.0/Microsoft.VisualStudio.Web.CodeGeneration.Msbuild.dll");
+            fileProvider.Copy(msbuildTaskDllPath, "toolAssets/net6.0/Microsoft.VisualStudio.Web.CodeGeneration.Msbuild.dll");
 
             var rootProjectTxt = MsBuildProjectStrings.SimpleNet50ProjectText;
             fileProvider.Add(MsBuildProjectStrings.RootProjectName, rootProjectTxt);

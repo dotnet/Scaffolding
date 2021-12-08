4099e449c3f046c4b420cd574ece572210c23bfc
Deep Choudhery <54324771+deepchoudhery@users.noreply.github.com>
2021-12-07 16:03:35 -0800

    not adding/forcing Account.Logout.cshtml if blazor project already adds it. (#1721)

--- src/Scaffolding/VS.Web.CG.Mvc/Identity/IdentityGeneratorTemplateModel2.cs
+++ src/Scaffolding/VS.Web.CG.Mvc/Identity/IdentityGeneratorTemplateModel2.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
@@ -11,5 +11,7 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity
         public string ContentVersion { get; set; }
 
         public bool IsRazorPagesProject { get; set; }
+
+        public bool IsBlazorProject { get; set; }
     }
 }

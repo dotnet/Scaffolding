f1a42b7af2da6f1340a79c097f4c3ff81494bbbc
Christopher Yeleighton <ne01026@shark.2a.pl>
2021-12-13 14:01:28 +0100

    Add tests for MsBuildProjectContextBuilder

---
+++ test/Shared/Microsoft.Extensions.ProjectModel.Tests/MsBuildProjectContextBuilderTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
+using Microsoft.DotNet.Scaffolding.Shared;
+using Microsoft.Extensions.ProjectModel;
+using Xunit;
+
+namespace Microsoft.Extensions.ProjectModel.Tests
+{
+    public class MsBuildProjectContextBuilderTests
+    {
+        [Fact]
+        public async Task ProjectArgumentExceptionTest()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new MsBuildProjectContextBuilder(String.Empty, "build"));
+            Assert.Equal(MessageStrings.ProjectPathNotGiven, ex.Message);
+        }
+
+        [Fact]
+        public async Task ProjectArgumentExceptionTest()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new MsBuildProjectContextBuilder("project", String.Empty));
+            Assert.Equal(MessageStrings.TargetLocationNotGiven, ex.Message);
+        }
+   }
+}

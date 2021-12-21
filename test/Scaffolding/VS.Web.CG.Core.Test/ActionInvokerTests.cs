48bf1d20423a9f8bd2508f81b7dcc7a9de786a2c
Christopher Yeleighton <ne01026@shark.2a.pl>
2021-12-22 00:13:59 +0100

    [WiP] Make sure ActionInvoker throws with InnerException (#1727)
    * Chain caught into InvalidOperationException
    
    * ActionInvoker_Throws_With_Inner_Exception
    
    * Apply coding guidelines
    
    * use Assert.Equal

--- test/Scaffolding/VS.Web.CG.Core.Test/ActionInvokerTests.cs
+++ test/Scaffolding/VS.Web.CG.Core.Test/ActionInvokerTests.cs
@@ -50,6 +50,41 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.Core.Test
             Assert.True(invokedModel.BoolProperty);
         }
 
+                [Fact]
+        public void ActionInvoker_Throws_With_Inner_Exception()
+        {
+            //Arrange
+            const string NOT_IMPLEMENTED_MESSAGE = "This action is intentionally not implemented.";
+            var codeGenInstance = new CodeGeneratorSample((model) =>
+            {
+                throw new NotImplementedException(NOT_IMPLEMENTED_MESSAGE);
+            });
+            
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            var generatorMock = new Mock<CodeGeneratorDescriptor>(typeof(CodeGeneratorSample).GetTypeInfo(),
+                serviceProviderMock.Object);
+
+            generatorMock
+                .SetupGet(cd => cd.CodeGeneratorInstance)
+                .Returns(codeGenInstance);
+            generatorMock
+                .SetupGet(cd => cd.Name)
+                .Returns(typeof(CodeGeneratorSample).Name);
+
+            var actionDescriptor = new ActionDescriptor(generatorMock.Object,
+                typeof(CodeGeneratorSample).GetMethod("GenerateCode")); //This is not a perfect unit test as the arrange is using actual instance rather than a mock
+
+            var actionInvoker = new ActionInvoker(actionDescriptor);
+
+            //Act
+            var ex =
+                Assert.Throws<InvalidOperationException>(
+                () => actionInvoker.Execute("CodeGeneratorSample StringValuePassed --BoolProperty".Split(' ')));
+
+            //Assert
+            Assert.Equal(NOT_IMPLEMENTED_MESSAGE, ex.InnerException.Message);
+         }
+
         private class CodeGeneratorSample
         {
             private Action<CodeGeneratorModel> _generateCodeImpl;

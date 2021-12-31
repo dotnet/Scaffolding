69a862ed2d37d217a68f2df163db7e7cc4776b94
Christopher Yeleighton <ne01026@shark.2a.pl>
2021-12-31 18:10:59 +0100

    using System.Threading.Tasks

--- src/Scaffolding/VS.Web.CG/CodeGenCommand.cs
+++ src/Scaffolding/VS.Web.CG/CodeGenCommand.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.DotNet.Scaffolding.Shared;
 
 namespace Microsoft.VisualStudio.Web.CodeGeneration

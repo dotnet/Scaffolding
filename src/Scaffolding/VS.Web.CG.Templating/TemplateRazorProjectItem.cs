6d69b1ae7bb0391f13022e553441cbb2d58cfcbe
Christopher Yeleighton <ne01026@shark.2a.pl>
2022-01-10 14:02:44 +0100

    TemplateRazorProjectItem: add path
    The bogus path `/Template` did not help me to find the template causing compilation error.

--- src/Scaffolding/VS.Web.CG.Templating/TemplateRazorProjectItem.cs
+++ src/Scaffolding/VS.Web.CG.Templating/TemplateRazorProjectItem.cs
@@ -10,23 +10,25 @@ namespace Microsoft.VisualStudio.Web.CodeGeneration.Templating
     internal class TemplateRazorProjectItem : RazorProjectItem
     {
         private readonly byte[] _contentUTF8Bytes;
+        private readonly FileInfo _path;
 
-        public TemplateRazorProjectItem(string content)
+        public TemplateRazorProjectItem(string path, string content)
         {
             var preamble = Encoding.UTF8.GetPreamble();
 
             var contentBytes = Encoding.UTF8.GetBytes(content);
 
-            _contentUTF8Bytes = new byte[preamble.Length + contentBytes.Length];
+            this._contentUTF8Bytes = new byte[preamble.Length + contentBytes.Length];
             preamble.CopyTo(_contentUTF8Bytes, 0);
             contentBytes.CopyTo(_contentUTF8Bytes, preamble.Length);
+            this._path = new FileInfo(path);
         }
 
-        public override string BasePath => "/";
+        public override string BasePath => this._path.Directory.FullName;
 
-        public override string FilePath => "Template";
+        public override string FilePath => this._path.Name;
 
-        public override string PhysicalPath => "/Template";
+        public override string PhysicalPath => this._path.FullName;
 
         public override bool Exists => true;
 

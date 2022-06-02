4234da99a4e4ad4ea7032ea9abc3d8a4cd242712
Zachary Halzel <zahalzel@microsoft.com>
2022-06-02 12:51:53 -0700

    Refactor appsettings.json modification logic (#1903)
    * refactor appSettings
    
    * Refactoring appsettings modifier
    
    * refactor appSettings and ServerAPI block
    
    * Refactor appsettings and tests
    
    * PR comments

--- src/MSIdentityScaffolding/Microsoft.DotNet.MSIdentity/AuthenticationParameters/AzureAdProperties.cs
+++ src/MSIdentityScaffolding/Microsoft.DotNet.MSIdentity/AuthenticationParameters/AzureAdProperties.cs
@@ -1,3 +1,6 @@
+using System;
+using Newtonsoft.Json.Linq;
+
 namespace Microsoft.DotNet.MSIdentity.AuthenticationParameters
 {
     public class PropertyNames
@@ -29,9 +32,110 @@ namespace Microsoft.DotNet.MSIdentity.AuthenticationParameters
         public const string ClientSecret = "Client secret from app-registration. Check user secrets/azure portal.";
 
         public const string Authority = "https://login.microsoftonline.com/22222222-2222-2222-2222-222222222222";
-        public const string ValidateAuthority = "true";
+        public const bool ValidateAuthority = true;
 
         public const string MicrosoftGraphBaseUrl = "https://graph.microsoft.com/v1.0";
-        public const string MicrosoftGraphScopes = "user.read";
+        public const string DefaultScopes = "user.read";
+    }
+
+    public class AzureAdBlock
+    {
+        public bool IsBlazorWasm;
+        public bool IsWebApi;
+
+        public string? ClientId;
+        public string? Instance = DefaultProperties.Instance;
+        public string? Domain;
+        public string? TenantId;
+        public string? Authority;
+        public string? CallbackPath = DefaultProperties.CallbackPath;
+
+        public string? Scopes;
+
+        public string? ClientSecret = DefaultProperties.ClientSecret;
+        public string[]? ClientCertificates;
+
+        public AzureAdBlock(ApplicationParameters applicationParameters)
+        {
+            IsBlazorWasm = applicationParameters.IsBlazorWasm;
+            IsWebApi = applicationParameters.IsWebApi.GetValueOrDefault();
+
+            Domain = !string.IsNullOrEmpty(applicationParameters.Domain) ? applicationParameters.Domain : null;
+            TenantId = !string.IsNullOrEmpty(applicationParameters.TenantId) ? applicationParameters.TenantId : null;
+            ClientId = !string.IsNullOrEmpty(applicationParameters.ClientId) ? applicationParameters.ClientId : null;
+            Instance = !string.IsNullOrEmpty(applicationParameters.Instance) ? applicationParameters.Instance : null;
+            Authority = !string.IsNullOrEmpty(applicationParameters.Authority) ? applicationParameters.Authority : null;
+            CallbackPath = !string.IsNullOrEmpty(applicationParameters.CallbackPath) ? applicationParameters.CallbackPath : null;
+            Scopes = !string.IsNullOrEmpty(applicationParameters.CalledApiScopes) ? applicationParameters.CalledApiScopes : null;
+        }
+
+        /// <summary>
+        /// Updates AzureAdBlock object from existing appSettings.json
+        /// </summary>
+        /// <param name="azureAdToken"></param>
+        public AzureAdBlock UpdateFromJToken(JToken azureAdToken)
+        {
+            JObject azureAdObj = JObject.FromObject(azureAdToken);
+
+            ClientId ??= azureAdObj.Value<string>(PropertyNames.ClientId); // here, if the applicationparameters value is null, we use the existing app settings value
+            Instance ??= azureAdObj.Value<string>(PropertyNames.Instance);
+            Domain ??= azureAdObj.Value<string>(PropertyNames.Domain);
+            TenantId ??= azureAdObj.Value<string>(PropertyNames.TenantId);
+            Authority ??= azureAdObj.Value<string>(PropertyNames.Authority);
+            CallbackPath ??= azureAdObj.Value<string>(PropertyNames.CallbackPath);
+            Scopes ??= azureAdObj.Value<string>(PropertyNames.Scopes);
+            ClientSecret ??= azureAdObj.Value<string>(PropertyNames.ClientSecret);
+            ClientCertificates ??= azureAdObj.Value<string[]>(PropertyNames.ClientCertificates);
+
+            return this;
+        }
+
+        public dynamic BlazorSettings => new
+        {
+            ClientId = ClientId ?? DefaultProperties.ClientId, // here, if a value is null, we could use the default properties
+            Authority = Authority ?? (string.IsNullOrEmpty(Instance) || string.IsNullOrEmpty(TenantId) ? DefaultProperties.Authority : $"{Instance}{TenantId}"),
+            ValidateAuthority = true
+        };
+
+        public dynamic WebAppSettings => new
+        {
+            Instance = Instance ?? DefaultProperties.Instance,
+            Domain = Domain ?? DefaultProperties.Domain,
+            TenantId = TenantId ?? DefaultProperties.TenantId,
+            ClientId = ClientId ?? DefaultProperties.ClientId,
+            CallbackPath = CallbackPath ?? DefaultProperties.CallbackPath
+        };
+
+        public dynamic WebApiSettings => new
+        {
+            Instance = Instance ?? DefaultProperties.Instance,
+            Domain = Domain ?? DefaultProperties.Domain,
+            TenantId = TenantId ?? DefaultProperties.TenantId,
+            ClientId = ClientId ?? DefaultProperties.ClientId,
+            CallbackPath = CallbackPath ?? DefaultProperties.CallbackPath,
+            Scopes = Scopes ?? DefaultProperties.DefaultScopes,
+            ClientSecret = ClientSecret ?? DefaultProperties.ClientSecret,
+            ClientCertificates = ClientCertificates ?? Array.Empty<string>()
+        };
+
+        public JObject ToJObject()
+        {
+            if (IsBlazorWasm)
+            {
+                return JObject.FromObject(BlazorSettings);
+            }
+            if (IsWebApi)
+            {
+                return JObject.FromObject(WebApiSettings);
+            }
+
+            return JObject.FromObject(WebAppSettings);
+        }
+    }
+
+    public class ApiSettingsBlock
+    {
+        public string? BaseUrl;
+        public string? Scopes;
     }
 }

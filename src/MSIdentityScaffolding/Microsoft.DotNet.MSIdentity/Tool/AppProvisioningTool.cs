c201fba4c4085ae574d06e2aa91e8d546e008ff1
Zach Halzel <zahalzel@microsoft.com>
2022-07-12 10:53:34 -0700

    Change logging

--- src/MSIdentityScaffolding/Microsoft.DotNet.MSIdentity/Tool/AppProvisioningTool.cs
+++ src/MSIdentityScaffolding/Microsoft.DotNet.MSIdentity/Tool/AppProvisioningTool.cs
@@ -42,7 +42,7 @@ namespace Microsoft.DotNet.MSIdentity
         private ProjectDescriptionReader? _projectDescriptionReader;
         private ProjectDescriptionReader ProjectDescriptionReader => _projectDescriptionReader ??= new ProjectDescriptionReader(FilePaths);
 
-        public AppProvisioningTool(string commandName, ProvisioningToolOptions provisioningToolOptions, bool silent = false) // TODO silent is temporary
+        public AppProvisioningTool(string commandName, ProvisioningToolOptions provisioningToolOptions, bool silent = true) // TODO silent is temporary
         {
             CommandName = commandName;
             ProvisioningToolOptions = provisioningToolOptions;
@@ -304,7 +304,6 @@ namespace Microsoft.DotNet.MSIdentity
             output.Append(jsonResponse.Output);
             var response = new JsonResponse(CommandName, jsonResponse.State, output: output.ToString());
 
-            ConsoleLogger.LogJsonMessage(response);
         }
 
         /// <summary>

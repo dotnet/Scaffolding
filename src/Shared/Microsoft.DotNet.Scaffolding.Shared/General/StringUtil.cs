717baaf3ff988beb80c039eae2a730aabb3fb8ec
Deep Choudhery <decho@microsoft.com>
2023-08-15 13:41:06 -0700

    support netstandard2.0 for Scaffolding.Shared

--- src/Shared/Microsoft.DotNet.Scaffolding.Shared/General/StringUtil.cs
+++ src/Shared/Microsoft.DotNet.Scaffolding.Shared/General/StringUtil.cs
@@ -3,7 +3,7 @@
 
 using System;
 
-namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc
+namespace Microsoft.DotNet.Scaffolding.Shared
 {
     internal static class StringUtil
     {
@@ -19,6 +19,11 @@ namespace Microsoft.VisualStudio.Web.CodeGenerators.Mvc
                 return input.Substring(0, length: 1).ToLowerInvariant() + input.Substring(1);
             }
             return input;
+        }
+
+        public static bool ContainsIgnoreCase(this string input, string value)
+        {
+            return input.ToLowerInvariant().Equals(value.ToLowerInvariant());
         }
     }
 }

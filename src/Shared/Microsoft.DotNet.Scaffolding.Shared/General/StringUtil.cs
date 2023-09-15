7cee7439a915815574a99a558c0921de97626c6f
Deep Choudhery <54324771+deepchoudhery@users.noreply.github.com>
2023-09-15 11:40:47 -0700

    supporting netstandard2.0 for Microsoft.DotNet.Scaffolding.Shared (#2530)
    * support netstandard2.0 for Scaffolding.Shared
    
    * fix StringUtil.ContainsIgnoreCase

---
+++ src/Shared/Microsoft.DotNet.Scaffolding.Shared/General/StringUtil.cs
@@ -0,0 +1,38 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.DotNet.Scaffolding.Shared
+{
+    internal static class StringUtil
+    {
+        public static string ToLowerInvariantFirstChar(this string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (input != string.Empty)
+            {
+                return input.Substring(0, length: 1).ToLowerInvariant() + input.Substring(1);
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// since netstandard2.0 does not have a Contains that allows StringOrdinal param, using lower invariant comparison.
+        /// </summary>
+        /// <returns>true if lower invariants are equal, false otherwise. false for any null scenario.</returns>
+        public static bool ContainsIgnoreCase(this string input, string value)
+        {
+            if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(value))
+            {
+                return false;
+            }
+
+            return (input?.ToLowerInvariant().Contains(value?.ToLowerInvariant())).GetValueOrDefault();
+        }
+    }
+}

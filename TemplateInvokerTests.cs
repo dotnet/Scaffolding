d033262f35bd963f8f8fb7060006e3b0316cb4cb
Deep Choudhery <decho@microsoft.com>
2023-11-01 14:56:30 -0700

    added ITemplateInvoker unit test

---
+++ TemplateInvokerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Microsoft.DotNet.Scaffolding.Shared.Tests
+{
+    public class TemplateInvokerTests
+    {
+
+        [Fact]
+        public void TestInvokeTemplate()
+        {
+
+        }
+    }
+}

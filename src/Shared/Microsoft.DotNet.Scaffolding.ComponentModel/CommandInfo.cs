f22c421baf4e7fbcb818095bdafb7fef08b1eb7a
Deep Choudhery <decho@microsoft.com>
2024-05-08 14:26:52 -0700

    added 'required' modifiers to CommandInfo and Parameter, added some unit tests for their deserialization.

--- src/Shared/Microsoft.DotNet.Scaffolding.ComponentModel/CommandInfo.cs
+++ src/Shared/Microsoft.DotNet.Scaffolding.ComponentModel/CommandInfo.cs
@@ -1,17 +1,16 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.DotNet.Scaffolding.ComponentModel
+namespace Microsoft.DotNet.Scaffolding.ComponentModel;
+
+/// <summary>
+/// What we want components to initialize and return (to stdout serialized as json) when 'get-commands' is invoked.
+/// Should be serialized as 'List<CommandInfo>'
+/// </summary>
+public class CommandInfo
 {
-    /// <summary>
-    /// What we want components to initialize and return (to stdout serialized as json) when 'get-commands' is invoked.
-    /// Should be serialized as 'List<CommandInfo>'
-    /// </summary>
-    public class CommandInfo
-    {
-        public string Name { get; set; } = default!;
-        public string DisplayName { get; set; } = default!;
-        public string? Description { get; set; }
-        public Parameter[] Parameters { get; set; } = default!;
-    }
+    public required string Name { get; init; }
+    public required string DisplayName { get; init; }
+    public string? Description { get; set; }
+    public required Parameter[] Parameters { get; init; }
 }
---
+++ test/Shared/Microsoft.DotNet.Scaffolding.ComponentModel.Tests/CommandInfoTests.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.DotNet.Scaffolding.ComponentModel.Tests
+{
+    public class CommandInfoTests
+    {
+        private static List<CommandInfo> CommandInfos;
+        private static Parameter ValidParameter;
+        private static Parameter PartialParameter;
+        private static Parameter PartialParameter2;
+        private static CommandInfo ValidCommandInfo;
+        private static CommandInfo PartialCommandInfo;
+        private static CommandInfo PartialCommandInfo2;
+
+        public CommandInfoTests()
+        {
+            ValidParameter = new()
+            {
+                Name = "parameter",
+                DisplayName = "Param Display Name",
+                Required = true,
+                Description = "Parameter description",
+                Type = BaseTypes.String,
+                PickerType = InteractivePickerType.DbProviderPicker
+            };
+
+            PartialParameter = new()
+            {
+                Name = string.Empty,
+                DisplayName = "Param Display Name 2",
+                Required = true,
+                Type = BaseTypes.Int
+            };
+
+            PartialParameter2 = new()
+            {
+                Name = "parameter2",
+                DisplayName = "Param Display Name 3",
+                Required = true,
+                Type = BaseTypes.Int,
+                Description = null,
+                PickerType = null
+            };
+
+            ValidCommandInfo = new()
+            {
+                Name = "command1",
+                DisplayName = "Command 1",
+                Description = "Description 1",
+                Parameters = [ValidParameter]
+            };
+
+            PartialCommandInfo = new()
+            {
+                Name = "command2",
+                DisplayName = "Command 2",
+                Parameters =
+                [
+                    ValidParameter, PartialParameter
+                ]
+            };
+
+            PartialCommandInfo2 = new()
+            {
+                Name = "command3",
+                DisplayName = "Command 3",
+                Description = null,
+                Parameters =
+                [
+                    ValidParameter, PartialParameter, PartialParameter2
+                ]
+            };
+
+            CommandInfos =
+            [
+                ValidCommandInfo, PartialCommandInfo, PartialCommandInfo2
+            ];
+        }
+    
+        [Fact]
+        public void SerialzationAndDeserializationTests()
+        {
+            var serializedJson = System.Text.Json.JsonSerializer.Serialize(CommandInfos);
+            Assert.False(string.IsNullOrEmpty(serializedJson));
+            Assert.True(serializedJson.Contains("\"Name\":\"command1\"", System.StringComparison.OrdinalIgnoreCase));
+            Assert.True(serializedJson.Contains("\"Name\":\"command2\"", System.StringComparison.OrdinalIgnoreCase));
+            Assert.True(serializedJson.Contains("\"Name\":\"command3\"", System.StringComparison.OrdinalIgnoreCase));
+
+            var deserializedJson = System.Text.Json.JsonSerializer.Deserialize<List<CommandInfo>>(serializedJson);
+            Assert.NotNull(deserializedJson);
+            Assert.True(deserializedJson.Count == 3);
+
+            var validCommandInfoDeserialized = deserializedJson[0];
+            var partialCommandInfoDeserialized = deserializedJson[1];
+            var partialCommandInfo2Deserialized = deserializedJson[2];
+            Assert.NotNull(validCommandInfoDeserialized);
+            Assert.NotNull(partialCommandInfoDeserialized);
+            Assert.NotNull(partialCommandInfo2Deserialized);
+
+            Assert.Contains(validCommandInfoDeserialized.Parameters, x => x.Name.Equals("parameter"));
+            Assert.Contains(partialCommandInfoDeserialized.Parameters, x => x.Name.Equals(PartialParameter.Name));
+            Assert.Contains(partialCommandInfo2Deserialized.Parameters, x => x.Description is null);
+            Assert.Contains(partialCommandInfo2Deserialized.Parameters, x => x.PickerType is null);
+        }
+    }
+}

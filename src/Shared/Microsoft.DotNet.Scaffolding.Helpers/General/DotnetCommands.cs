fe9e501518783e9d93dc12a128e7579e1748b052
Tim Mulholland <timulhol@microsoft.com>
2024-05-16 22:27:56 -0700

    Fix nit

--- src/Shared/Microsoft.DotNet.Scaffolding.Helpers/General/DotnetCommands.cs
+++ src/Shared/Microsoft.DotNet.Scaffolding.Helpers/General/DotnetCommands.cs
@@ -38,6 +38,7 @@ public static class DotnetCommands
                 {
                     logger.LogMessage($"\n{stdErr}");
                 }
+
                 logger.LogMessage("Failed!");
             }
             else

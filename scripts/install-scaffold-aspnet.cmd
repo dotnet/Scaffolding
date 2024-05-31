7a2eb6b1332d601a41ebb541a32f12c7be5d534f
Deep Choudhery <54324771+deepchoudhery@users.noreply.github.com>
2024-05-30 22:46:28 -0700

    move projects out of `tools` folder (#2778)
    * move projects out of tools
    
    * revert dotnet sdk to .NET 9 P4
    
    * moved Microsoft.DotNet.Scaffolding.Helpers and Microsoft.DotNet.Scaffolding.ComponentModel

--- scripts/install-scaffold-aspnet.cmd
+++ scripts/install-scaffold-aspnet.cmd
@@ -4,7 +4,7 @@ set SRC_DIR=%cd%
 set NUPKG=artifacts/packages/Debug/Shipping/
 call taskkill /f /im dotnet.exe
 call rd /Q /S artifacts
-call dotnet pack tools\dotnet-scaffold-aspnet\dotnet-scaffold-aspnet.csproj -c Debug
+call dotnet pack src\dotnet-scaffolding\dotnet-scaffold-aspnet\dotnet-scaffold-aspnet.csproj -c Debug
 call dotnet tool uninstall -g Microsoft.dotnet-scaffold-aspnet
 
 call cd %DEFAULT_NUPKG_PATH%
--- scripts/install-scaffold.cmd
+++ scripts/install-scaffold.cmd
@@ -4,7 +4,7 @@ set SRC_DIR=%cd%
 set NUPKG=artifacts/packages/Debug/Shipping/
 call taskkill /f /im dotnet.exe
 call rd /Q /S artifacts
-call dotnet pack tools\dotnet-scaffold\dotnet-scaffold.csproj -c Debug
+call dotnet pack src\dotnet-scaffolding\dotnet-scaffold\dotnet-scaffold.csproj -c Debug
 call dotnet tool uninstall -g Microsoft.dotnet-scaffold
 
 call cd %DEFAULT_NUPKG_PATH%

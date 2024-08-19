13f74a529afdc3a8c90f8784875fac7d5660f6e7
Deep Choudhery <decho@microsoft.com>
2024-08-19 13:38:09 -0700

    update jquery files

--- src/Scaffolding/VS.Web.CG.Mvc/Templates/Identity/Bootstrap5/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
+++ src/Scaffolding/VS.Web.CG.Mvc/Templates/Identity/Bootstrap5/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
@@ -1,6 +1,10 @@
-// Unobtrusive validation support library for jQuery and jQuery Validate
-// Copyright (c) .NET Foundation. All rights reserved.
-// @version v3.2.11
+/**
+ * @license
+ * Unobtrusive validation support library for jQuery and jQuery Validate
+ * Copyright (c) .NET Foundation. All rights reserved.
+ * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+ * @version v4.0.0
+ */
 
 /*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: false */
 /*global document: false, jQuery: false */
@@ -10,7 +14,7 @@
         // AMD. Register as an anonymous module.
         define("jquery.validate.unobtrusive", ['jquery-validation'], factory);
     } else if (typeof module === 'object' && module.exports) {
-        // CommonJS-like environments that support module.exports
+        // CommonJS-like environments that support module.exports     
         module.exports = factory(require('jquery-validation'));
     } else {
         // Browser global

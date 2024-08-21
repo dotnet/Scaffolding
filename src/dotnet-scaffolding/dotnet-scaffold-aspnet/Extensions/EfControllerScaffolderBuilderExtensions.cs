501398b95062416cc88092059ac9754e3cf4adad
Deep Choudhery <decho@microsoft.com>
2024-08-21 12:51:02 -0700

    minor edits

--- src/dotnet-scaffolding/dotnet-scaffold-aspnet/Extensions/EfControllerScaffolderBuilderExtensions.cs
+++ src/dotnet-scaffolding/dotnet-scaffold-aspnet/Extensions/EfControllerScaffolderBuilderExtensions.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-using Microsoft.Build.Definition;
 using Microsoft.DotNet.Scaffolding.CodeModification;
 using Microsoft.DotNet.Scaffolding.Core.Builder;
 using Microsoft.DotNet.Scaffolding.Core.Steps;

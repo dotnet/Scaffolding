3eff8317d404dfe0224e33edfda4efb786515b96
Deep Choudhery <decho@microsoft.com>
2024-11-06 12:40:53 -0800

    when displaying categories, change "All" to "(Show All)"

---
+++ src/dotnet-scaffolding/Microsoft.DotNet.Scaffolding.Core/ComponentModel/ScaffolderConstants.cs
@@ -0,0 +1,8 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace Microsoft.DotNet.Scaffolding.Core.ComponentModel;
+
+internal static class ScaffolderConstants
+{
+    internal const string DEFAULT_CATEGORY = "All";
+}

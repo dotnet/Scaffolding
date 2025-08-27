299f6b7e5886fee96c93ac9eba7f5888aa099942
Phil Henning <phenning@microsoft.com>
2025-08-27 11:12:02 -0400

    Update ModelTypesLocatorTestWeb App to use WebApplication Builder to unblock arcade PR

--- test/Scaffolding/TestApps/ModelTypesLocatorTestWebApp/Program.cs
+++ test/Scaffolding/TestApps/ModelTypesLocatorTestWebApp/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ModelTypesLocatorTestWebApp.Models;
 
 namespace ModelTypesLocatorTestWebApp
 {
@@ -13,17 +7,17 @@ namespace ModelTypesLocatorTestWebApp
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .ConfigureLogging(loggerFactory => loggerFactory
-                    .AddConsole()
-                    .AddDebug())
-                .UseStartup<Startup>()
-                .Build();
+            var builder = WebApplication.CreateBuilder(args);
 
-            host.Run();
+            builder.Logging.AddConsole();
+            builder.Logging.AddDebug();
+
+            builder.Services.AddDbContext<CarContext>(options =>
+                options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CarContext-3ae1a974-3117-4483-853a-06d90fdb3bd0;Trusted_Connection=True;MultipleActiveResultSets=true"));
+
+            var app = builder.Build();
+
+            app.Run();
         }
     }
 }

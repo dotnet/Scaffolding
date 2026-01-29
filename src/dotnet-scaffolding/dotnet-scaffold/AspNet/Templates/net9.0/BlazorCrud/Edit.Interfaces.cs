da61579fc3505108e9ef5e60adc27a064cffa19f
haileymck <hmckelvie@microsoft.com>
2026-01-28 16:04:39 -0800

    Add aspnet net 9 templates to templates\net9.0

---
+++ src/dotnet-scaffolding/dotnet-scaffold/AspNet/Templates/net9.0/BlazorCrud/Edit.Interfaces.cs
@@ -0,0 +1,8 @@
+using Microsoft.DotNet.Scaffolding.TextTemplating;
+
+namespace Microsoft.DotNet.Tools.Scaffold.AspNet.Templates.BlazorCrud;
+
+public partial class Edit : ITextTransformation
+{
+}
+

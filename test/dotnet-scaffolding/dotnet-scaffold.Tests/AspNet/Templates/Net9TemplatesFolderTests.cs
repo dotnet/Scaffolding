0504ab1c4489083a43ab542bd149f7a90d854f7f
haileymck <hmckelvie@microsoft.com>
2026-02-05 10:21:11 -0800

    update

--- test/dotnet-scaffolding/dotnet-scaffold.Tests/AspNet/Templates/Net9TemplatesFolderTests.cs
+++ test/dotnet-scaffolding/dotnet-scaffold.Tests/AspNet/Templates/Net9TemplatesFolderTests.cs
@@ -14,6 +14,7 @@ public class Net9TemplatesFolderTests
     [
         "BlazorCrud",
         "BlazorIdentity",
+        "CodeModificationConfigs",
         "EfController",
         "Files",
         "Identity",
